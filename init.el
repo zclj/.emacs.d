1ac096bf1020710bef1ea3dae8eeef8d42d90775
Stefan Karlsson <info-s@tryne.se>
2016-11-16 21:12:26 +0100

    Backup files are put in a common dir

--- init.el
+++ init.el
@@ -286,3 +286,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;---------------------------
+;; Common place for Backup files
+;;---------------------------
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )

243930241e1b4e696465299c89ebf470f73e62b0
Stefan Karlsson <info-s@tryne.se>
2018-12-21 14:07:17 +0100

    Big update

--- init.el
+++ init.el
@@ -1,156 +1,187 @@
 ;-----------
 ; Packages
-; ----------
+;-----------
 (require 'package)
-
+ 
 (add-to-list 'package-archives
               '("melpa" . "https://melpa.org/packages/"))
-
+ 
 (package-initialize)
-
+ 
 (defvar my-packages
   '(;; Clojure
+
+    ;; New test Clojure
+    ;;clojure-mode
+    ;;inf-clojure
+    
+    ;; Old Clojure
     cider
     clj-refactor
-    clojure-cheatsheet
+    ;;clojure-cheatsheet
     helm-cider
-    cider-eval-sexp-fu
     clojure-snippets
-    
+ 
+    ;; Web
+    web-mode
+   
     ;; Navigation
     helm
     avy
     ace-window
-
+ 
     ;; Project navigation
     projectile
     helm-projectile
-    
+   
     ;; List
     rainbow-delimiters
     paredit
-
+ 
     ;; Doc
     which-key
-    
+   
     ;; Auto complete
     company
     company-flx
 
+    ;; Document formats
+    markdown-mode
+    
     ;; Misc
     magit
     move-text
-    expand-region))
-
+    buffer-move
+    expand-region
+    smart-mode-line
+    exec-path-from-shell))
+ 
 (when (not package-archive-contents)
   (package-refresh-contents))
-
+ 
 ;; Automaticaly install any missing packages
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
-
+ 
 ;; Load key bindings.
 (load (concat user-emacs-directory "keybinds.el"))
 
+;;----------------------------------------
+;; Proxy
+;;----------------------------------------
+(setq url-proxy-services '(("no_proxy" . "proxy.se.abb.com")
+                           ("http" . "proxy.se.abb.com:81")
+                           ("https" . "proxy.se.abb.com:81")))
+ 
 ;;----------------------------------------
 ;; expand region - form aware selection
 ;;----------------------------------------
 (require 'expand-region)
 (global-set-key (kbd "C-@") 'er/expand-region)
-
+ 
 ;;----------------------------------------
 ;; move text
 ;; It allows you to move the current line using M-up / M-down
 ;; if a region is marked, it will move the region instead.
 ;;----------------------------------------
-
+ 
 (require 'move-text)
 (move-text-default-bindings)
+
+;;----------------------------------------
+;; Markdown
+;;----------------------------------------
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+  "Major mode for editing GitHub Flavored Markdown files" t)
+
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "52d43c63ce3d612053fc9f98a13ecd60fbe116095a41c1b90c76556519044f19" default)))
+ '(initial-buffer-choice t)
+ '(markdown-command "/usr/local/bin/pandoc")
+ '(package-selected-packages
+   (quote
+    (docker-compose-mode solarized-theme web-mode zenburn-theme which-key rainbow-delimiters move-text magit helm-projectile helm-cider expand-region company-flx clojure-snippets clojure-cheatsheet clj-refactor cider-eval-sexp-fu ace-window))))
 
 ;;----------------------------------------
 ;; Docs
 ;;----------------------------------------
 (which-key-mode)
-
+ 
 ;;----------------------------------------
 ;; ace window
 ;;----------------------------------------
-(global-set-key (kbd "M-p") 'ace-window)
+;;(global-set-key (kbd "M-p") 'ace-window)
+
+;; Trying to use default 'other-window' binding
+(global-set-key (kbd "C-x o") 'ace-window)
+
 ;; set to home row instead of default 0-9
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-
+ 
 ;;----------------------------------------
 ;; avy
 ;;----------------------------------------
 ;; TODO : provar så här..
 (global-set-key (kbd "C-c j") 'avy-goto-word-or-subword-1)
+(global-set-key (kbd "C-;") 'avy-goto-char-2)
 ;;(global-set-key (kbd "M-p") 'avy-goto-char-2)
 ;; (global-set-key (kbd "s-.") 'avy-goto-word-or-subword-1)
 ;; (global-set-key (kbd "s-w") 'ace-window)
-
-
-;; TODO - REMOVE if avy works out
-;;----------------------------------------
-;; ace jump mode
-;;----------------------------------------
-;; ace jump mode major function
-;; 
-;(add-to-list 'load-path "/full/path/where/ace-jump-mode.el/in/")
-;; (autoload
-;;   'ace-jump-mode
-;;   "ace-jump-mode"
-;;   "Emacs quick move minor mode"
-;;   t)
-
-;; you can select the key you prefer to
-;;(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
-
-
-;; 
-;; enable a more powerful jump back function from ace jump mode
-;;
-;; (autoload
-;;   'ace-jump-mode-pop-mark
-;;   "ace-jump-mode"
-;;   "Ace jump back:-)"
-;;   t)
-;; (eval-after-load "ace-jump-mode"
-;;   '(ace-jump-mode-enable-mark-sync))
-;; (define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
-
-;;----------------------------------------
-;; Workgroups2
-;;----------------------------------------
-;(require 'workgroups2)
-
-;;(setq wg-session-load-on-start t)    ; default: (not (daemonp))
-
-;; Change prefix key (before activating WG)
-;(setq wg-prefix-key (kbd "C-c z"))
-
-;; Change workgroups session file
-;(setq wg-session-file "~/.emacs.d/.emacs_workgroups")
-
-;; Set your own keyboard shortcuts to reload/save/switch WGs:
-;; "s" == "Super" or "Win"-key, "S" == Shift, "C" == Control
-;(global-set-key (kbd "<pause>")     'wg-reload-session)
-;(global-set-key (kbd "C-S-<pause>") 'wg-save-session)
-;(global-set-key (kbd "s-z")         'wg-switch-to-workgroup)
-;(global-set-key (kbd "s-/")         'wg-switch-to-previous-workgroup)
-
-;(workgroups-mode 1)
-
+ 
+  
 ;;----------------------------------------
 ;; Magit
 ;;----------------------------------------
 (global-set-key (kbd "C-x g") 'magit-status)
+ 
+;;----------------------------------------
+;; React
+;; https://code-and-cocktails.herokuapp.com/blog/2016/01/10/emacs-setup-for-react-slash-reactnative/
+;;----------------------------------------
+;;(add-to-list 'auto-mode-alist '(".*/react/.*\\.js[x]?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
+(setq web-mode-markup-indent-offset 2
+      web-mode-css-indent-offset 2
+      web-mode-code-indent-offset 2)
+ 
+(setq js-indent-level 2)
+ 
+(setq web-mode-content-types-alist
+  '(("jsx" . "\\.js[x]?\\'")))
 
 ;;----------------------------------------
-;; Clojure
+;; Clojure - Test new
+;;----------------------------------------
+;; (add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
+;; (add-hook 'clojure-mode-hook 'paredit-mode)
+
+;; (add-hook 'clojure-mode-hook #'eldoc-mode)
+;; (add-hook 'inf-clojure-mode-hook #'eldoc-mode)
+
+;; (setf inf-clojure-tools-deps-cmd '("localhost" . 5555))
+
+
+;;----------------------------------------
+;; Clojure - OLD
 ;;----------------------------------------
 (require 'cider)
 (require 'clj-refactor)
+;;(require 'cider-eval-sexp-fu)
 
 (defun clojure-mode-hook ()
   (paredit-mode)
@@ -158,25 +189,18 @@
   (yas-minor-mode 1) ; for adding require/use/import statements
   ;; This choice of keybinding leaves cider-macroexpand-1 unbound
   (cljr-add-keybindings-with-prefix "C-c C-m"))
-
-(require 'cider-eval-sexp-fu)
-
-;; Eldoc displays function signatures in the minibuffer as you're typing. 
+  
+;;Eldoc displays function signatures in the minibuffer as you're typing.
 (add-hook 'cider-repl-mode-hook #'eldoc-mode)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
 
+;;(add-hook 'clojure-mode-hook #'eldoc-mode)
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'clojure-mode-hook #'clojure-mode-hook)
 
-;;----------------
-;; squiggly-clojure / flycheck
-;;----------------
-;; (eval-after-load 'flycheck '(flycheck-clojure-setup))
-;; (add-hook 'after-init-hook #'global-flycheck-mode)
-
-;; (eval-after-load 'flycheck
-;;   '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+;;helm cider
+(helm-cider-mode 1)
 
 ;;----------------
 ;; helm
@@ -188,112 +212,148 @@
 (define-key global-map [remap list-buffers] 'helm-buffers-list)
 (define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") #'helm-buffers-list)
+
 (unless (boundp 'completion-in-region-function)
   (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
   (define-key emacs-lisp-mode-map       [remap completion-at-point] 'helm-lisp-completion-at-point))
-
-;; helm cider
-(helm-cider-mode 1)
-
+  
 ;; helm projectile
 (require 'helm-projectile)
 (helm-projectile-on)
-
+ 
 ;---------------
 ; auto-complete
 ;----------------
-; new auto-complete
 (global-company-mode)
-
+ 
 ;; turn on company-flx for fuzzy search
 (with-eval-after-load 'company
   (company-flx-mode +1))
-
+ 
 (global-set-key (kbd "TAB") #'company-indent-or-complete-common)
 (global-set-key [M-tab] 'company-complete)
-
-;---------------
-; Zenburn-theme
-;---------------
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'zenburn t)
-
+ 
 ;;--------------
 ;; Visual
 ;;--------------
 ; Set font
+;; (add-to-list 'default-frame-alist
+;;              '(font . "DejaVu Sans Mono-14"))
+
 (add-to-list 'default-frame-alist
-             '(font . "DejaVu Sans Mono-12"))
+             '(font . "Menlo 16"))
+
+;; theme
+(load-theme 'solarized-dark t)
+
+;; Highlight paren pairs
+(show-paren-mode 1)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; use line numbers globaly
+(global-display-line-numbers-mode)
+
+;; turn of sound and blink warnings
+(setq ring-bell-function 'ignore)
+(setq visible-bell 1)
+ 
+;; turn of bars
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
+  
+(add-hook `text-mode-hook 'turn-on-visual-line-mode)
+
+;; smart mode line
+(sml/setup)
+
+
+;; hideshow blocks
+(load-library "hideshow")
+(setq hs-hide-comments t)
+(add-hook 'prog-mode-hook 'hs-minor-mode)
+
+;; rainbow parens when programming
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;; if two files with same name is open show dir instead of <n>
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
 
 ;------------
 ; Misc stuff
 ;------------
+;; Are we on a mac?
+(setq is-mac (equal system-type 'darwin))
 
-;; turn off tool bar
-(tool-bar-mode -1)
+;; Setup environment variables from the user's shell.
+(when is-mac
+  (exec-path-from-shell-initialize))
 
-;; using shift+arrow to move between windows
-(windmove-default-keybindings)
+;; use aspell instead of ispell
+(setq ispell-program-name "aspell")
 
-(add-hook `text-mode-hook 'turn-on-visual-line-mode)
-
+;; the scratch buffer starts empty
 (setq initial-scratch-message nil)
 
-(projectile-global-mode)
+;; save location of point for next time the a file is opened
+(save-place-mode 1)
 
+;; save backup files in .emacs.d
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))
+
+;------------
+; Projectile
+;------------
+(projectile-global-mode)
+ 
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+ 
 ;--------
 ; Octave
 ;--------
 (autoload 'octave-mode "octave-mod" nil t)
 (setq auto-mode-alist
       (cons '("\\.m$" . octave-mode) auto-mode-alist))
-
+ 
 (add-hook 'octave-mode-hook
           (lambda ()
             (abbrev-mode 1)
             (auto-fill-mode 1)
             (if (eq window-system 'x)
                 (font-lock-mode 1))))
-
+ 
 ;----------------
 ; Org-mode
 ;----------------
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-
+ 
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 (setq org-log-done t)
-
+ 
 ;;---------------------------
 ;; Global Clipboard support
 ;;---------------------------
 (global-set-key "\C-w" 'clipboard-kill-region) ;; \C-w
 (global-set-key "\M-w" 'clipboard-kill-ring-save) ;; \M-w
 (global-set-key "\C-y" 'clipboard-yank) ;;\C-y
+ 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("52d43c63ce3d612053fc9f98a13ecd60fbe116095a41c1b90c76556519044f19" default)))
- '(initial-buffer-choice t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;;---------------------------
-;; Common place for Backup files
-;;---------------------------
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-  backup-by-copying t    ; Don't delink hardlinks
-  version-control t      ; Use version numbers on backups
-  delete-old-versions t  ; Automatically delete excess backups
-  kept-new-versions 20   ; how many of the newest versions to keep
-  kept-old-versions 5    ; and how many of the old
-  )
--- keybinds.el
+++ keybinds.el
@@ -1,14 +1,42 @@
+;; key bindings for MacOS
+(when (eq system-type 'darwin) ;; mac specific settings
+  (setq mac-option-modifier 'super)
+  (setq mac-command-modifier 'meta)
+  (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
+  )
+
 (global-set-key [f12] (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
-
+ 
 (require 'projectile)
-
+ 
 (defun find-project-file (file)
   (find-file (expand-file-name file (projectile-project-root))))
-
+ 
 (global-set-key [f9]
                 (lambda () (interactive)
                   (find-project-file "dev/user.clj")))
-
+ 
 (global-set-key [f10]
                 (lambda () (interactive)
 		  (find-project-file "project.clj")))
+
+;; using shift+arrow to move between windows
+(windmove-default-keybindings)
+
+;; Swap current buffer with buffer in direction of arrow
+(global-set-key (kbd "<M-s-right>") 'buf-move-right)
+(global-set-key (kbd "<M-s-left>") 'buf-move-left)
+(global-set-key (kbd "<M-s-up>") 'buf-move-up)
+(global-set-key (kbd "<M-s-down>") 'buf-move-down)
+
+;; hide/show
+(global-set-key (kbd "C-.") 'hs-toggle-hiding)
+(global-set-key (kbd "C-,") 'hs-hide-all)
+(global-set-key (kbd "C-x C-,") 'hs-show-all)
+
+;; Font size
+(define-key global-map (kbd "C-+") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+;; Revert without any fuss
+(global-set-key (kbd "M-<escape>") (lambda (revert-buffer t t)))

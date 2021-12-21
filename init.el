e93fcf5932db89016d11508c29cd5bbacf30dd08
Stefan <info-s@tryne.se>
2021-12-21 19:05:11 +0100

    Updates to Haskell mode

--- init.el
+++ init.el
@@ -242,11 +242,14 @@
 (require 'haskell-interactive-mode)
 (require 'haskell-process)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
 
 (custom-set-variables
-  '(haskell-process-suggest-remove-import-lines t)
-  '(haskell-process-auto-import-loaded-modules t)
-  '(haskell-process-log t))
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-stylish-on-save t)
+ '(haskell-tags-on-save t))
 
 
 ;---------------
